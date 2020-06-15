b79bbfe7546d6d8c801ce8a15c13cbc07e3b93d3
Trần Duy Thanh <coachtranduythanh@gmail.com>
2020-06-15 16:27:14 +0700

    update version of pygame

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ source sandbox-flappybird/bin/activate
 # sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.7.1-cp27-none-linux_x86_64.whl
 # sudo pip install --upgrade https://storage.googleapis.com/tensorflow/mac/tensorflow-0.7.1-cp27-none-any.whl
 pip2 install tensorflow==0.12.0rc0
-pip2 install pygame
+pip2 install pygame==2.0.0.dev4
 pip2 install opencv-python
 
 python2 deep_q_network.py
